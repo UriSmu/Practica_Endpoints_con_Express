4ab20b9058ab88eacf871d6eb5b65afe8e5b2551
UriSmu <48240113@ort>
2025-04-11 09:05:54 -0300

    avances

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-console.log("algún texto");
+import express from "express";
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.send("Hello world!");
+})
+
+app.get('/saludo', (req, res) => {
+    res.send("Hola!");
+})
+
+app.get('/numero', (req, res) => {
+    res.send((42).toString());
+})
+
+app.get('/usuario', (req, res) => {
+    res.send({"nombre": "Ana", "edad": 25 });
+})
+
+app.get('/productos', (req, res) => {
+    res.send([ "Mouse", "Teclado", "Monitor" ]);
+})
+
+app.get('/materias', (req, res) => {
+    res.send([ { "nombre": "Matemática" }, { "nombre": "Lengua" } ] );
+})
+
+app.listen(port, () => {
+    console.log(`Listening on http://localhost:${port}`);
+})
